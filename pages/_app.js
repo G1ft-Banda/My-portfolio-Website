9421021f39a42cbfb0eaa51d13059efb3a25c7fb
gift <bandaolga19.com>
2022-09-29 11:30:52 +0200

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/d14a45b8f3.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
